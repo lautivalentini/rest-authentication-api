cc6e17ba532ac63c6f803806e0f9b391c492d1b4
Lautaro Valentini <lautiivalentini@gmail.com>
2021-05-12 16:42:57 -0300

    add search categories and products

--- rest-server/controllers/searchController.js
+++ rest-server/controllers/searchController.js
@@ -7,7 +7,6 @@ const collectionsAllowed = [
   'users',
   'categories',
   'products',
-  'roles'
 ]
 
 const searchUsers = async (term = "", res = response) => {
@@ -27,6 +26,34 @@ const searchUsers = async (term = "", res = response) => {
   res.json({ results: users })
 }
 
+const searchCategories = async (term = "", res = response) => {
+  const isMongoId = isValidObjectId( term )
+
+  if ( isMongoId ) {
+    const category = await Category.findById( term )
+    return res.json( (category) ? [ category ] : [] )
+  }
+
+  const regex = new RegExp( term, 'i' )
+
+  const categories = await Category.find({ name: regex, state: true })
+  res.json({ results: categories })
+}
+
+const searchProducts = async (term = "", res = response) => {
+  const isMongoId = isValidObjectId( term )
+
+  if ( isMongoId ) {
+    const product = await Product.findById( term ).populate('category', 'name')
+    return res.json( (product) ? [ product ] : [] )
+  }
+
+  const regex = new RegExp( term, 'i' )
+
+  const products = await Product.find({ name: regex, state: true }).populate('category', 'name')
+  res.json({ results: products })
+}
+
 const search = ( req = request , res = response ) => {
   const { collection, term } = req.params
 
@@ -39,10 +66,11 @@ const search = ( req = request , res = response ) => {
       searchUsers(term, res)
     break;
     case 'categories':
-
+      searchCategories(term, res)
     break;
     case 'products':
-
+      searchProducts(term, res)
+    break;
     default:
       res.status(500).json({msg: 'Server error'})
   }
